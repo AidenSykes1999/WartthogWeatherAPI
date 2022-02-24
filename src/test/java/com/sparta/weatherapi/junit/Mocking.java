f73c40c6899a67f1eb7c023d4c4d5c9d514728b9
mehmetyetim <98969298+mehmetyetim@users.noreply.github.com>
2022-02-24 15:31:05 +0000

    structure of packages changed under test

--- src/test/java/com/sparta/weatherapi/junit/Mocking.java
+++ src/test/java/com/sparta/weatherapi/junit/Mocking.java
@@ -1,37 +1,30 @@
-package com.spartaglobal.warthog.JUnit;
+package com.sparta.weatherapi.junit;
 
-import com.sparta.weatherapi.Injector;
-import com.sparta.weatherapi.weatherjson.Sys;
-import com.sparta.weatherapi.weatherjson.Weather;
-import org.junit.Before;
+import com.sparta.weatherapi.dtoweather.SystemInformation;
+import com.sparta.weatherapi.dtoweather.Weather;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Mocking {
 
-    public static Weather weth;
+    public static Weather weather;
 
     @BeforeAll
     public static void mockSetUp()
     {
         //creates a base class to mock using a given example before the tests are ran
-        weth = mockSetUp.createWeather("responseExample.txt");
+        weather = mockSetUp.createWeather("responseExample.txt");
     }
 
     @Test
     @DisplayName("Given that the provided example gives FR as the country. Check that this is correctly returned")
     public void testCorrectCountry()
     {
-        Sys sys  = weth.getSys();
-        Sys sys2 = Mockito.spy(sys);
+        SystemInformation sys  = weather.getSystemInformation();
+        SystemInformation sys2 = Mockito.spy(sys);
         Mockito.when(sys2.getCountry()).thenReturn("FR");
 
         Assertions.assertTrue(sys2.getCountry().equals("FR"));
