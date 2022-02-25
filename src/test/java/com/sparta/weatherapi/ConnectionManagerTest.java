26907a2bbccd42369b8b2ff79e4cd86b570d03dd
StephenJones1997 <98388181+StephenJones1997@users.noreply.github.com>
2022-02-25 14:23:22 +0000

    Upgrading the coverage of our code

--- src/test/java/com/sparta/weatherapi/ConnectionManagerTest.java
+++ src/test/java/com/sparta/weatherapi/ConnectionManagerTest.java
@@ -1,8 +1,10 @@
 package com.sparta.weatherapi;
 
 import com.sparta.weatherapi.dtoweather.Weather;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -60,4 +62,120 @@ public class ConnectionManagerTest {
 
     }
 
+
+    @Test
+    @DisplayName("Given Language is not provides, give the correct response ")
+    public void cityApiCallNoLanguage() {
+        setUp();
+        String weatherString = connectionManager.cityApiCall("Beijing", "CN-BJ", "86");
+        Weather weather = injector.injectResponseToWeather(weatherString);
+
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
+    @Test
+    @DisplayName("Given State is not provides, and only City and state give the correct response ")
+    public void cityApiCallNoState() {
+        setUp();
+
+        String weatherString = connectionManager.cityApiCall("New York", "US-NY");
+        Weather weather = injector.injectResponseToWeather(weatherString);
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
+    @Test
+    @DisplayName("Given State is not provides, and only City, state give the correct response ")
+    public void cityApiCallNoStateLanguage() {
+        setUp();
+
+        String weatherString = connectionManager.cityApiCall("New York", "US-NY");
+        Weather weather = injector.injectResponseToWeather(weatherString);
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Given State is not provides, and only City, state and Units give the correct response ")
+    public void cityApiCallNoStatewUnits() {
+        setUp();
+
+        String weatherString = connectionManager.cityApiCall("New York", "US-NY", Units.IMPERIAL);
+        Weather weather = injector.injectResponseToWeather(weatherString);
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
+    @Test
+    @DisplayName("Given State is not provides, and only City, state and Language give the correct response ")
+    public void cityApiCallNoStatewLanguage() {
+        setUp();
+
+        String weatherString = connectionManager.cityApiCall("New York", "US-NY", Languages.FRENCH);
+        Weather weather = injector.injectResponseToWeather(weatherString);
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
+    @Test
+    @DisplayName("Given State is not provides, and only City, state, Units and Language give the correct response ")
+    public void cityApiCallNoStatewUnitsLanguage() {
+        setUp();
+
+        String weatherString = connectionManager.cityApiCall("New York", "US-NY", Units.IMPERIAL, Languages.FRENCH);
+        Weather weather = injector.injectResponseToWeather(weatherString);
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("City only is provided give the correct response ")
+    public void cityApiCallCityOnly() {
+        setUp();
+
+        String weatherString = connectionManager.cityApiCall("New York");
+        Weather weather = injector.injectResponseToWeather(weatherString);
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("City only is provided with a language give the correct response ")
+    public void cityApiCallCityOnlyLanguage() {
+        setUp();
+
+        String weatherString = connectionManager.cityApiCall("New York", Languages.BULGARIAN);
+        Weather weather = injector.injectResponseToWeather(weatherString);
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("City only is provided with a Unit give the correct response")
+    public void cityApiCallCityOnlyUnits() {
+        setUp();
+
+        String weatherString = connectionManager.cityApiCall("New York", Units.STANDARD);
+        Weather weather = injector.injectResponseToWeather(weatherString);
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("City only is provided with a Unit and Language give the correct response")
+    public void cityApiCallCityOnlyUnitsandLanguage() {
+        setUp();
+
+        String weatherString = connectionManager.cityApiCall("New York", Units.STANDARD, Languages.FRENCH);
+        Weather weather = injector.injectResponseToWeather(weatherString);
+        System.out.println(weather);
+
+        Assertions.assertEquals(200, connectionManager.getStatusCode());
+    }
 }
--- src/test/java/com/sparta/weatherapi/junit/Mocking.java
+++ src/test/java/com/sparta/weatherapi/junit/Mocking.java
@@ -1,8 +1,11 @@
 package com.sparta.weatherapi.junit;
 
+import com.sparta.weatherapi.ConnectionManager;
+import com.sparta.weatherapi.dtoweather.Coordinates;
 import com.sparta.weatherapi.dtoweather.SystemInformation;
 import com.sparta.weatherapi.dtoweather.Weather;
 
+import com.sparta.weatherapi.dtoweather.Wind;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -21,14 +24,40 @@ public class Mocking {
     }
 
     @Test
+    @DisplayName("Given a mock connection, confirm that the class is working and returns the string")
+    public void mockConnection()
+    {
+        ConnectionManager mock = Mockito.mock(ConnectionManager.class);
+        Mockito.when(mock.cityApiCall("London")).thenReturn("Working");
+
+        Assertions.assertTrue(mock.cityApiCall("London") == "Working");
+    }
+    @Test
     @DisplayName("Given that the provided example gives FR as the country. Check that this is correctly returned")
     public void testCorrectCountry()
     {
-        SystemInformation sys  = weather.getSystemInformation();
-
-        SystemInformation sys2 = Mockito.spy(sys);
+        SystemInformation sys2 = Mockito.spy(weather.getSystemInformation());
         Mockito.when(sys2.getCountry()).thenReturn("FR");
-
         Assertions.assertTrue(sys2.getCountry().equals("FR"));
+    }
+
+    @Test
+    @DisplayName("Given that the provided example has long and latitude of 100,100. Check that ToString is returned correctly")
+    public void testCorrectLONGLAT()
+    {
+        Coordinates sys2 = Mockito.spy(weather.getCoordinates());
+        Mockito.when(sys2.toString()).thenReturn("Coordinates: { longitude: 100°, latitude: 100°}");
+        Assertions.assertTrue(sys2.toString().equals("Coordinates: { longitude: 100°, latitude: 100°}"));
+    }
+
+
+
+    @Test
+    @DisplayName("Given that the provided example has wind speed of 50. Check that getSpeed is returned correctly")
+    public void testCorrectWind()
+    {
+        Wind sys2 = Mockito.spy(weather.getWind());
+        Mockito.when(sys2.getSpeed()).thenReturn(50.0);
+        Assertions.assertTrue(sys2.getSpeed() == 50.0);
     }
 }
